0b4c1bb159a78f4b5e14ea127f95b01a730d835e
DunesWake <SquigerDoo4243>
2020-02-27 22:54:41 -0700

    Crawler optimizations

--- crawler.py
+++ crawler.py
@@ -4,17 +4,12 @@ from bs4 import BeautifulSoup
 from urllib.request import Request, urlopen
 
 
-def remove_last_line_from_string(s):
-    return s[:s.rfind('\n')]
-
-
 def recursion(url):
     req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
     web_byte = urlopen(req).read()
     rawPage = web_byte.decode('utf-8')
     soup = BeautifulSoup(rawPage, 'html.parser')
-    for tag in soup.find_all("div", class_="c-postList__post__thumbnail"):
-
+    for tag in soup.find_all("div", class_="c-postList__post__title c-postList__post__title__wo_dek"):
         # FOR EACH ARTICLE ON RESULTS PAGE
         for link in tag.find_all('a'):
             print(link.get('href'))
@@ -28,8 +23,8 @@ def recursion(url):
             for text in article.find_all("p"):
                 # since each line is scanned in this is a bit redundancy
                 # also might be removing some article if in last line
-                str = remove_last_line_from_string(text.get_text().replace('jsaltz@villagevoice.com', ''))
-                if len(str) != 0:
+                str = text.get_text()
+                if len(str) != 0 and not ('More:' in str) and not ('jsaltz@villagevoice.com' in str):
                     print(str)
 
     for t in soup.find_all("a", class_="next page-numbers"):
@@ -39,6 +34,6 @@ def recursion(url):
             return
 
 
-url = 'https://www.villagevoice.com/author/jerrysaltz/page/34/'
+url = 'https://www.villagevoice.com/author/jerrysaltz/'
 recursion(url)
 
